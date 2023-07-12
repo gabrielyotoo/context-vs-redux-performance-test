6cbd8bb5522db9ddea13fe93a0b367b38604f28d
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-07-12 19:35:24 -0300

    feat: improve styles

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,38 @@ export default function App() {
 
   return (
     <main>
-      <section>
-        <h1>React Context API</h1>
-        <button onClick={() => setRenderContext(!renderContext)}>
-          Toggle Render Context
-        </button>
-        <CounterProvider>
-          {renderContext
-            ? Array.from({ length: 10000 }, () => <Context />)
-            : null}
-        </CounterProvider>
-      </section>
-      <section>
-        <h1>Redux</h1>
-        <button onClick={() => setRenderRedux(!renderRedux)}>
-          Toggle Render Redux
-        </button>
-        <Provider store={store}>
-          {renderRedux ? Array.from({ length: 10000 }, () => <Redux />) : null}
-        </Provider>
-      </section>
+      <button
+        onClick={() => {
+          setRenderRedux(!renderRedux);
+          setRenderContext(!renderContext);
+        }}
+      >
+        Toggle render both
+      </button>
+      <div className="round">
+        <section>
+          <h1>React Context API</h1>
+          <button onClick={() => setRenderContext(!renderContext)}>
+            Toggle Render Context
+          </button>
+          <CounterProvider>
+            {renderContext
+              ? Array.from({ length: 10000 }, () => <Context />)
+              : null}
+          </CounterProvider>
+        </section>
+        <section>
+          <h1>Redux</h1>
+          <button onClick={() => setRenderRedux(!renderRedux)}>
+            Toggle Render Redux
+          </button>
+          <Provider store={store}>
+            {renderRedux
+              ? Array.from({ length: 10000 }, () => <Redux />)
+              : null}
+          </Provider>
+        </section>
+      </div>
     </main>
   );
 }
